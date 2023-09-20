575425a6badd4e872eb312105df2e3bf18365020
Haorsis <cadetgauthier3@gmail.com>
2023-09-20 16:38:27 +0200

    init

---
+++ src/test/java/fr/erasmus/socialNetwork/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.erasmus.socialNetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
