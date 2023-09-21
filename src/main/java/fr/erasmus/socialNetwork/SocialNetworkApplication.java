5158a3faf503b69a6158686f5e587eb33fd07073
Haorsis <cadetgauthier3@gmail.com>
2023-09-21 09:58:41 +0200

    # Logging # properties files

--- src/main/java/fr/erasmus/socialNetwork/SocialNetworkApplication.java
+++ src/main/java/fr/erasmus/socialNetwork/SocialNetworkApplication.java
@@ -1,13 +1,18 @@
 package fr.erasmus.socialNetwork;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SocialNetworkApplication {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SocialNetworkApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
+		LOGGER.info("Application started");
 	}
 
 }
